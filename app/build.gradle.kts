765e14240a2a44962ec454a10e58e9d5f80b7b7a
Marco Mata <612310094@utsalamanca.edu.mx>
2025-03-31 21:21:12 -0600

    Actualización.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.implementation
-
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.googleGmsGoogleServices)
@@ -50,7 +48,4 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
 }
